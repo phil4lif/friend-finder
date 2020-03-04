42bc43d5a2ee7e72e793896f8282bdfc724b553e
phil cowan <philipjcowan1@gmail.com>
2020-03-04 15:57:45 -0700

    adds more friends to the array

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,9 +15,14 @@ var friends = [
         responses: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     },
     {
-        name: "Navi",
-        photolink: "images\navi.jpg",
+        name: "Dale",
+        photolink: "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg",
         responses: [3, 4, 5, 3, 4, 5, 3, 4, 5 , 5]
+    },
+    {
+        name: "Bernard",
+        photolink: "https://image.cnbcfm.com/api/v1/image/106207101-1572264314938rts2t5ab.jpg?v=1572264369&w=678&h=381",
+        responses: [4, 5, 3, 4, 5, 2, 3, 4, 5, 5,]
     }
 ];
 
